b4f845ac0d3774aff29efa096e84aba5507fb19c
Juan Diego Obando Zúñiga <85593827+jdotaz1@users.noreply.github.com>
2023-10-08 19:49:52 -0500

    Update 3Fibo.cpp

--- Laboratorio3/3Fibo.cpp
+++ Laboratorio3/3Fibo.cpp
@@ -5,7 +5,7 @@ void fibo(int n){
   int n1 = 1;
   int n2 = 1;
   cout << n1 << " "<< n2;
-  for(int i = 0;i < n - 2; i++){
+  for(int i = 0;i < n - 1; i++){
     int a = n1 + n2;
     n1 = n2;
     n2 = a;
